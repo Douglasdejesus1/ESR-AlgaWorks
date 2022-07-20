bf81b65c65ca157091095fc5ff0de181e0782908
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-07-20 08:56:14 -0300

    @ConfigurationPropeties

--- algafood-api/src/main/java/com/douglas/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/douglas/algafood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,6 @@
 package com.douglas.algafood.di.notificacao;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +10,13 @@ import com.douglas.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	@Value("${configuration.email.conection}")
+	/*@Value("${configuration.email.conection}")
 	private String host;
 	
 	@Value("${configuration.email.port}")
-	private Integer porta;
+	private Integer porta;*/
+	@Autowired
+	public NotificadorProperties notificadorProperties;
 	
 	public NotificadorEmail() {
 		System.out.println("PRODUCAO");
@@ -23,8 +25,8 @@ public class NotificadorEmail implements Notificador {
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Host: "+host);
-		System.out.println("Porta: "+porta);
+		System.out.println("Host: "+notificadorProperties.getHostServidor());
+		System.out.println("Porta: "+notificadorProperties.getPortaServidor());
 		System.out.printf("Notificando %s atraves do e-mail %s:  %s\n",
 				cliente.getNome(), cliente.getEmail(),mensagem);
 	}
