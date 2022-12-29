4be4db1cf493274794a0c114cefbebb99b94aea3
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-12-29 20:39:14 -0300

    5.15. Tornando a consulta com Criteria API com filtros dinâmicos

--- algafood-project-jpa/src/main/java/com/douglas/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-project-jpa/src/main/java/com/douglas/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.douglas.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -33,8 +37,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 				.setParameter("taxaFinal", taxaFreteFinal)
 				.getResultList();
 	}*/
+	
+	//consulta dinamica JPQL
+	/*
 	@Override
-	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, 
+			BigDecimal taxaFreteFinal) {
 
 		var jpql = new StringBuilder();
 
@@ -56,10 +64,61 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		}
 		
 		// TypedQuery é uma consulta tipada
-		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), 
+				Restaurante.class);
 		parametros.forEach((chave,valor)->query.setParameter(chave, valor));
 		
 		return query.getResultList();
-	}
+	}*/
+	
+	//5.14 - Consulta com CRITERIA API ESTATIVO
+	/*public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, 
+			BigDecimal taxaFreteFinal) {
+		
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		//consulta simples
+		//criteria.from(Restaurante.class);
+		
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		Predicate nomePredicate = builder.like(root.get("nome"), "%"+nome+"%");
+		
+		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+		
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		return query.getResultList();
+	}*/
+	//5.15 - Consulta com CRITERIA API DINAMICO
+		public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, 
+				BigDecimal taxaFreteFinal) {
+			
+			CriteriaBuilder builder = manager.getCriteriaBuilder();
+			
+			var predicates  = new ArrayList<Predicate>();
+			
+			CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+			
+			
+			Root<Restaurante> root = criteria.from(Restaurante.class);
+			
+			if(StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%"+nome+"%"));
+			}
+			if(taxaFreteInicial !=null) {
+				predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+			}
+			if(taxaFreteFinal !=null) {
+				predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+			}
+			
+			criteria.where(predicates.toArray(new Predicate[0]));
+			
+			TypedQuery<Restaurante> query = manager.createQuery(criteria);
+			return query.getResultList();
+		}
 
 }
