586cf39bdb87bf6ffe04c5bf3f4b7707cdb97889
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-01-14 19:44:00 -0300

    9.16. Criando constraints de validação customizadas com implementação de ConstraintValidator

--- algafood-project-jpa/src/main/java/com/douglas/algafood/domain/model/Restaurante.java
+++ algafood-project-jpa/src/main/java/com/douglas/algafood/domain/model/Restaurante.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -27,6 +26,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.douglas.algafood.core.validation.Groups;
+import com.douglas.algafood.core.validation.Multiplo;
 import com.douglas.algafood.core.validation.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -50,6 +50,7 @@ public class Restaurante {
 
 	//@PositiveOrZero uso no lugar, a titulo de exemplo:
 	@TaxaFrete
+	@Multiplo(numero = 5)
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
