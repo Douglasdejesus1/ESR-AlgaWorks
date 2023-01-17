86348bf5897b3c5329af28fd6edac5f25ab0e4fc
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-01-16 21:56:19 -0300

    10.3. Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ + refatorando para versão anterior do springboot

--- algafood-project-testes-integracao/src/main/java/com/douglas/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
+++ algafood-project-testes-integracao/src/main/java/com/douglas/algafood/infrastructure/repository/CustomJpaRepositoryImpl.java
@@ -1,18 +1,11 @@
 package com.douglas.algafood.infrastructure.repository;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
 import javax.persistence.EntityManager;
 
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 
 import com.douglas.algafood.domain.repository.CustomJpaRepository;
 
