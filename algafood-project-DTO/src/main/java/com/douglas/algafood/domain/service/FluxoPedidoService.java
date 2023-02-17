e3e976aad8f402492af6a027b7e88a4c535f4a2a
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-02-16 22:41:21 -0300

    12.22. Implementando endpoint de transição de status de pedidos

---
+++ algafood-project-DTO/src/main/java/com/douglas/algafood/domain/service/FluxoPedidoService.java
@@ -0,0 +1,61 @@
+package com.douglas.algafood.domain.service;
+
+import java.time.OffsetDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douglas.algafood.domain.exception.NegocioException;
+import com.douglas.algafood.domain.model.Pedido;
+import com.douglas.algafood.domain.model.StatusPedido;
+
+@Service
+public class FluxoPedidoService {
+
+	@Autowired
+	private EmissaoPedidoService emissaoPedido;
+	
+	
+	@Transactional
+	public void confirmar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		
+		if(!pedido.getStatus().equals(StatusPedido.CRIADO)) {
+			throw new NegocioException(
+					String.format("Stratus do pedido %s não pode ser alterado de %s para %s",pedidoId,
+							pedido.getStatus(),StatusPedido.CONFIRMADO));
+		}
+		pedido.setStatus(StatusPedido.CONFIRMADO);
+		pedido.setDataConfirmacao(OffsetDateTime.now());	
+	}
+	
+	@Transactional
+	public void cancelar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		
+		if(!pedido.getStatus().equals(StatusPedido.CANCELADO)) {
+			throw new NegocioException(
+					String.format("Stratus do pedido %s não pode ser alterado de %s para %s",pedidoId,
+							pedido.getStatus(),StatusPedido.CANCELADO));
+		}
+		pedido.setStatus(StatusPedido.CANCELADO);
+		pedido.setDataCancelamento(OffsetDateTime.now());	
+	}
+	@Transactional
+	public void entregar(Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		
+		if(!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
+			throw new NegocioException(
+					String.format("Stratus do pedido %s não pode ser alterado de %s para %s",pedidoId,
+							pedido.getStatus(),StatusPedido.ENTREGUE));
+		}
+		pedido.setStatus(StatusPedido.ENTREGUE);
+		pedido.setDataEntrega(OffsetDateTime.now());	
+	}
+}
