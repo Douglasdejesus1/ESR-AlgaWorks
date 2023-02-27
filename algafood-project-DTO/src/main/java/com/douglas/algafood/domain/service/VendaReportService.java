443adc9a5b0fe165d6827df6a6a12f927ff6a3cd
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-02-26 23:29:24 -0300

    13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- algafood-project-DTO/src/main/java/com/douglas/algafood/domain/service/VendaReportService.java
+++ algafood-project-DTO/src/main/java/com/douglas/algafood/domain/service/VendaReportService.java
@@ -3,8 +3,7 @@ package com.douglas.algafood.domain.service;
 import com.douglas.algafood.domain.filter.VendaDiariaFilter;
 
 public interface VendaReportService {
-	
-	
-	byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
 
+	byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
+	
 }
---
+++ algafood-project-DTO/src/main/java/com/douglas/algafood/infrastructure/service/report/ReportException.java
@@ -0,0 +1,17 @@
+package com.douglas.algafood.infrastructure.service.report;
+
+public class ReportException extends RuntimeException {
+
+	public ReportException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ReportException(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
