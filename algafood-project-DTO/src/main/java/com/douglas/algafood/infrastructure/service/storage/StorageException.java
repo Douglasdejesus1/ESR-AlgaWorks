053e30b5bff4fc8d3f0cb7afd21698cf1fd9aaa6
Douglasdejesus1 <douglas.dj1242@gmail.com>
2023-03-12 16:30:39 -0300

    14.8. Implementando o serviço de armazenagem de fotos no disco local

---
+++ algafood-project-DTO/src/main/java/com/douglas/algafood/infrastructure/service/storage/StorageException.java
@@ -0,0 +1,16 @@
+package com.douglas.algafood.infrastructure.service.storage;
+
+public class StorageException extends RuntimeException{
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StorageException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
